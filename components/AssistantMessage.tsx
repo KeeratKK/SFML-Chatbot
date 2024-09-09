159c40946cc4f2cd82bda847463d55fd86216ea5
Keerat Kohli <100006952+KeeratKK@users.noreply.github.com>
2024-09-09 18:55:48 -0400

    Added Functionality for Markdown Parsing

--- components/AssistantMessage.tsx
+++ components/AssistantMessage.tsx
@@ -1,6 +1,7 @@
 import { Message } from "@/types/messages";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Markdown from "react-markdown";
 
 interface AssistantMessageProps {
     message: Message;
@@ -9,6 +10,7 @@ interface AssistantMessageProps {
 
 const AssistantMessage: React.FC<AssistantMessageProps> = ({ message, idx }) => {
 
+    // Looks for code blocks wrapped in ```cpp
     const blocks = message.content.split(/(```[\w]*\n[\s\S]*?\n```)/g);
 
     return (
@@ -34,8 +36,20 @@ const AssistantMessage: React.FC<AssistantMessageProps> = ({ message, idx }) => 
                       </SyntaxHighlighter>  
                     );
                 }
-
-                return <p key={indx}>{block}</p>
+                
+                return (
+                  <Markdown key={indx}
+                    components = {{strong: ({node, ...props}) => (
+                      <>
+                        <br />
+                        <strong style={{color: 'black'}} {...props} />
+                      </>
+                    ),
+                  }}
+                  >
+                    {block}
+                  </Markdown>
+                );
 
             })}
 
